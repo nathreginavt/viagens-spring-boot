bbca22ec4478e33ecc72249a316f9bb6e0b7a008
Nathália Regina <nathalia.reginavt@gmail.com>
2024-05-06 21:37:33 -0300

    PostMapping

--- src/main/java/com/fatecrl/viagens/controller/ViagemController.java
+++ src/main/java/com/fatecrl/viagens/controller/ViagemController.java
@@ -1,35 +1,87 @@
 package com.fatecrl.viagens.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fatecrl.viagens.bean.Viagem;
 import com.fatecrl.viagens.service.ViagemService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/viagens")
 
 public class ViagemController {
-    
+
     @Autowired
     private ViagemService _viagemService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<Viagem> getViagemById(@PathVariable Long id){
-        if(id == null){
+    public ResponseEntity<Viagem> getViagemById(@PathVariable Long id) {
+        if (id == null) {
+            // status 400
             return ResponseEntity.badRequest().build();
-        } 
+        }
 
         Viagem trip = _viagemService.getViagemById(id);
-        
-        if(trip == null){
+
+        if (trip == null) {
+            // status 404
             return ResponseEntity.notFound().build();
         }
 
+        // status 200
         return ResponseEntity.ok(trip);
     }
+
+    @PostMapping("/")
+    public ResponseEntity<Viagem> postViagem(
+        @RequestBody Viagem viagem) {
+
+        // request invalido
+        if (viagem.getOrderNumber() == null ||
+                viagem.getAmount() == null ||
+                viagem.getSource() == null ||
+                viagem.getDestination() == null ||
+                viagem.getStartDateTime() == null ||
+                viagem.getEndDateTime() == null ||
+                viagem.getType() == null) {
+            // Return an error response indicating that required fields are missing
+            return ResponseEntity.badRequest().build();
+        }
+
+        // inconsistencias
+        if (viagem.getStartDateTime().isAfter(viagem.getEndDateTime())) {
+
+            // status 422
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        String source = viagem.getSource();
+        String destination = viagem.getDestination();
+        if(source.equalsIgnoreCase(destination)){
+            // status 422
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        Viagem trip = _viagemService
+        .postViagem(viagem);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(trip.getId())
+                .toUri();
+
+        // status 201
+        return ResponseEntity.created(location).build();
+    }
+
 }
--- src/main/java/com/fatecrl/viagens/service/ViagemService.java
+++ src/main/java/com/fatecrl/viagens/service/ViagemService.java
@@ -9,10 +9,19 @@ import com.fatecrl.viagens.bean.Viagem;
 
 @Service
 public class ViagemService {
-    List<Viagem> viagens = new ArrayList<Viagem>();
+    public static List<Viagem> viagens = new ArrayList<Viagem>();
 
     public ViagemService(){
+        fakeTrip();
+    }
 
+    private void fakeTrip(){
+        Viagem viagem1 = new Viagem();
+        viagem1.setSource("Brazil");
+        viagem1.setDestination("USA");
+        //viagem1.setType(RETURN);
+        viagem1.setId(1L);
+        viagens.add(viagem1);
     }
 
     public Viagem getViagemById(Long id){
@@ -24,13 +33,27 @@ public class ViagemService {
         return null;
     }
 
-    /* 
+    
     public List<Viagem> getViagens(){
         List<Viagem> tripsList = new ArrayList<Viagem>(viagens);
         return tripsList;
     }
-    */
-    
 
-    
+    public Viagem postViagem(Viagem viagem){
+        Viagem trip = new Viagem();
+
+        trip.setId(viagem.generateNextId());
+        trip.setOrderNumber(viagem.getOrderNumber());
+        trip.setAmount(viagem.getAmount());
+        trip.setSource(viagem.getSource());
+        trip.setDestination(viagem.getDestination());
+        trip.setStartDateTime(viagem.getStartDateTime());
+        trip.setEndDateTime(viagem.getEndDateTime());
+        trip.setType(viagem.getType());
+
+        viagens.add(trip);
+        
+        return trip;
+    }
+
 }
