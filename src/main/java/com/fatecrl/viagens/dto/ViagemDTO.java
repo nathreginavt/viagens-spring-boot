0fea4395e07aa33545bb360bed27623a83cf52b8
barbaragrinop <barbarapereira123@hotmail.com>
2024-06-19 21:34:10 -0300

    feature: adc lombok e altera DTO

--- src/main/java/com/fatecrl/viagens/dto/ViagemDTO.java
+++ src/main/java/com/fatecrl/viagens/dto/ViagemDTO.java
@@ -4,9 +4,12 @@ import java.time.LocalDateTime;
 
 import com.fatecrl.viagens.model.Type;
 
-public class ViagemDTO {
+import lombok.*;
 
-    private Long id;
+@Getter
+@Setter
+
+public class ViagemDTO {
 
     private String orderNumber;
 
@@ -21,68 +24,4 @@ public class ViagemDTO {
     private LocalDateTime endDateTime;
 
     private Type type;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getOrderNumber() {
-        return orderNumber;
-    }
-
-    public void setOrderNumber(String orderNumber) {
-        this.orderNumber = orderNumber;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public LocalDateTime getStartDateTime() {
-        return startDateTime;
-    }
-
-    public void setStartDateTime(LocalDateTime startDateTime) {
-        this.startDateTime = startDateTime;
-    }
-
-    public LocalDateTime getEndDateTime() {
-        return endDateTime;
-    }
-
-    public void setEndDateTime(LocalDateTime endDateTime) {
-        this.endDateTime = endDateTime;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
 }
